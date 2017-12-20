c9ae2060f35c31fbac71332624f266b20111bdf8
cares86 <caressavgroenebergh@live.nl>
2017-12-20 15:31:12 +0100

    add files

---
+++ julycheck.js
@@ -0,0 +1,15 @@
+var d = new Date();
+var md = d.getMonth();
+month(md);
+
+function month(d){
+
+
+
+if (d =='6') {
+   alert("It's July, so it can get really hot.");
+}
+else {
+    alert("It’s not July, so at least we’re not in the peak of the heat.");
+}
+}
